dc75a1856cbce90f9f2bfe3543a6f1c9fd8b818d
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2021-09-24 17:39:50 +0300

    add express server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,30 @@
 const gameloop = require('node-gameloop');
+const path = require('path');
 
-console.log('Hello from server!');
+import * as express from "express";
+const app = express.default();
+const port = 3000;
 
-const gameState = {
-    x: 0,
-    y: 0
-};
+app.use(express.static(path.join(__dirname, '..', 'client')));
 
-let frameCount = 0;
-const id = gameloop.setGameLoop((delta: number) => {
-    // game logic
-    // send state to users
-}, 1000 / 2);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Listening on: ${port}`);
+});
+
+const gameLoopStuff = () => {
+    const gameState = {
+        x: 0,
+        y: 0
+    };
+
+    let frameCount = 0;
+    const id = gameloop.setGameLoop((delta: number) => {
+        // game logic
+        // send state to users
+    }, 1000 / 2);
+
+}
